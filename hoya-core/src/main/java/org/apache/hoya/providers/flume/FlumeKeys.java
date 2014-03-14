24203ddb90ecc9bfaa34723d0976fe89280cc24e
keyki <keyki.kk@gmail.com>
2014-03-14 10:15:28 +0100

    removed unused key

--- hoya-core/src/main/java/org/apache/hoya/providers/flume/FlumeKeys.java
+++ hoya-core/src/main/java/org/apache/hoya/providers/flume/FlumeKeys.java
@@ -5,7 +5,6 @@ public interface FlumeKeys {
     String ROLE_AGENT = "agent";
     String FLUME_CONFIG = "flume-agent.xml";
     String PROVIDER_NAME = "flume";
-    String LOG_DIR = "LOG_DIR";
     String FLUME_NG_SCRIPT = "flume-ng";
     String AGENT_NAME = "agent.name";
     String AGENT_FILE = "agent.file";
